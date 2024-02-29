18258842f123155840f3c5eb8b276c3237431a3d
Nelamin2 <nidalelamin@gmail.com>
2024-02-29 13:40:03 -0800

    task

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -10,17 +10,16 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
-	binary_tree_t *parent1, *parent2;
+binary_tree_t *parent1, *parent2;
 
-	if (!first || !second)
-		return (NULL);
-	if (first == second)
-		return ((binary_tree_t *)first);
-
-	parent1 = first->parent, parent2 = second->parent;
-	if (first == parent2 || !parent1 || (!parent1->parent && parent2))
-		return (binary_trees_ancestor(first, parent2));
-	else if (parent1 == second || !parent2 || (!parent1->parent && parent1))
-		return (binary_trees_ancestor(parent1, second));
-	return (binary_trees_ancestor(parent1, parent2));
+if (!first || !second)
+return (NULL);
+if (first == second)
+return ((binary_tree_t *)first);
+parent1 = first->parent, parent2 = second->parent;
+if (first == parent2 || !parent1 || (!parent1->parent && parent2))
+return (binary_trees_ancestor(first, parent2));
+else if (parent1 == second || !parent2 || (!parent1->parent && parent1))
+return (binary_trees_ancestor(parent1, second));
+return (binary_trees_ancestor(parent1, parent2));
 }
